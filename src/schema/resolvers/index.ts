e9faa06ae80e43991a044326d1e489e6f392e34d
Ben Schwartz <ben.schwartz@cybergrx.com>
2020-07-22 18:52:40 -0600

    Stubbed out login and signup mutations

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -5,7 +5,24 @@ export const resolvers: any = {
   Query: {
     async me(parent: any, args: any, ctx: Context) {
       console.log(parent, args)
-      // return await ctx.prisma.user()
+      return await ctx.prisma.user.findMany()
+    }
+  },
+  Mutation: {
+    async loginUser(parent: any, args: any, ctx: Context) {
+      const { username, password } = args
+      return {
+        token: "token123",
+        userId: 1
+      }
+    },
+    async signupUser(parent: any, args: any, ctx: Context) {
+      const { data: { name, email, username, password } } = args
+      console.log(`signup: ${name} ${email} ${username} ${password}`)
+      return {
+        token: "token123",
+        userId: 1
+      }
     }
   },
   User: {
