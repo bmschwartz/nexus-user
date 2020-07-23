7e01a4babf931a4e2830e21f43aa29fe549a3900
Ben Schwartz <ben.schwartz@cybergrx.com>
2020-07-22 21:57:05 -0600

    Fleshed out the signupUser mutation. Updated the db user model

--- src/schema/resolvers/Mutation.ts
+++ src/schema/resolvers/Mutation.ts
@@ -10,11 +10,23 @@ export const Mutation = {
   },
 
   async signupUser(parent: any, args: any, ctx: Context) {
-    const { data: { name, email, username, password } } = args
-    console.log(`signup: ${name} ${email} ${username} ${password}`)
+    const { input: { name, email, username, password } } = args
+
+    let user = await ctx.prisma.user.findOne({ where: { email } })
+    if (user) {
+      throw new Error("A user with that email already exits")
+    }
+
+    user = await ctx.prisma.user.findOne({ where: { username } })
+    if (user) {
+      throw new Error("A user with that username already exits")
+    }
+
+    user = await ctx.prisma.user.create({ data: { name, email, username, password } })
+
     return {
-      token: "token123",
-      userId: 1
+      userId: user.id,
+      token: "temp-token"
     }
   }
 }
