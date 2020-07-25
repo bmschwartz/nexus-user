83593380f49ffecc79ceee3f980296977e26ff04
Ben Schwartz <ben.schwartz@cybergrx.com>
2020-07-25 12:56:04 -0600

    Removed log

--- src/schema/resolvers/User.ts
+++ src/schema/resolvers/User.ts
@@ -2,7 +2,6 @@ import { Context } from "../../context"
 
 export const UserResolvers = {
   async __resolveReference(user: any, ctx: Context) {
-    console.log(user)
     return await ctx.prisma.user.findOne({ where: { id: Number(user.id) } })
   },
 }
