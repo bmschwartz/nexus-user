9ab443cef30232470dfcf9a823ae7259e228d686
Ben Schwartz <ben.schwartz@cybergrx.com>
2020-07-29 11:24:44 -0600

    Adding to the seed.sql file for permissions

--- prisma/seed.sql
+++ prisma/seed.sql
@@ -17,11 +17,65 @@ VALUES ('password', 'rachel@example.com', 'rachel', FALSE);
 INSERT INTO "User" (password, email, username, admin)
 VALUES ('password', 'nic@example.com', 'nic', FALSE);
 
+
+/** Permission types */
 INSERT INTO "Permission" (name, description)
 VALUES ('site:admin', 'Allows access to everything in the site');
 
+INSERT INTO "Permission" (name, description)
+VALUES ('site:member', 'Registered member of the site');
+
+
+/** Assign site:admin */
 INSERT INTO "UserPermission" ("userId", "permissionId")
 VALUES (1, 1);
 
 INSERT INTO "UserPermission" ("userId", "permissionId")
 VALUES (2, 1);
+
+
+/** Assign site:member */
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (1, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (2, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (3, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (4, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (5, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (6, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (7, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (8, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (9, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (10, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (11, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (12, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (13, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (14, 2);
+
+INSERT INTO "UserPermission" ("userId", "permissionId")
+VALUES (15, 2);
