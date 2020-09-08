85dd5bcf8164cd26713e8b050dab0662d721a7b8
Ben Schwartz <schwartz.ben0@gmail.com>
2020-09-08 16:34:04 -0600

    Upgraded @prisma/cli

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE "nexus_users";
 CREATE TABLE "public"."User" (
   id SERIAL PRIMARY KEY NOT NULL,
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   username VARCHAR(255) UNIQUE NOT NULL,
-  admin BOOLEAN DEFAULT false,
-  "createdAt" TIMESTAMP NOT NULL DEFAULT now()
+  "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+  "updatedAt" TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE "public"."Permission" (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) UNIQUE NOT NULL,
   description VARCHAR(255) NOT NULL
 );
 
@@ -24,18 +24,3 @@ CREATE TABLE "public"."UserPermission" (
   FOREIGN KEY ("userId") REFERENCES "public"."User"(id),
   FOREIGN KEY ("permissionId") REFERENCES "public"."Permission"(id)
 );
-
-ALTER TABLE "public"."User"
-  DROP COLUMN admin;
-
-ALTER TABLE "public"."Permission"
-    ADD CONSTRAINT Permission_name_key
-    UNIQUE (name);
-
-ALTER TABLE "public"."User"
-  ADD CONSTRAINT User_email_key
-  UNIQUE (email);
-
-------- ADD DATE FIELDS -------
-ALTER TABLE "public"."User"
-  ADD COLUMN "updatedAt" TIMESTAMP NOT NULL DEFAULT now();
