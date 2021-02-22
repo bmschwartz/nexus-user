dcb8ee50708a3ce2d79e33f4bbda31e0f9417962
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-22 16:30:30 -0700

    Fix context parsing

--- src/context.ts
+++ src/context.ts
@@ -11,8 +11,8 @@ export interface Context {
 export function createContext({ req }: any): Context {
   let { userid: userId, permissions } = req.headers
 
-  userId = userId !== "undefined" ? userId : undefined
-  permissions = permissions !== "undefined" ? JSON.parse(permissions) : []
+  userId = (userId !== "undefined" && userId !== undefined) ? userId : undefined
+  permissions = (permissions !== "undefined" && permissions !== undefined) ? JSON.parse(permissions) : []
 
   return {
     prisma,
