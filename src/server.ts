f9ff525fdb345ae4d27f14d5f48846fa68892217
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-06 20:48:40 -0700

    New endpoint just to get a user's email from user id. Protected by an hmac signature

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import { ApolloServer } from "apollo-server"
+import bodyParser from "body-parser";
+import express, {Request, Response} from "express"
+import { ApolloServer } from "apollo-server-express"
 import { buildFederatedSchema } from "@apollo/federation"
 import { applyMiddleware } from "graphql-middleware"
 import Amplify, { Auth as AmplifyAuth } from "aws-amplify"
@@ -9,9 +11,13 @@ import { createContext } from "./context"
 import { permissions } from "./permissions"
 import { logger } from "./logger";
 import { awsExports } from "./aws-exports"
+import crypto from "crypto";
 
 Amplify.configure(awsExports)
 AmplifyAuth.configure(awsExports)
+
+const app = express()
+app.use(bodyParser.json())
 
 const server = new ApolloServer({
   schema: applyMiddleware(
@@ -25,6 +31,37 @@ const server = new ApolloServer({
   introspection: true,
 })
 
-server.listen({ port: 4003 }).then(({ url }) => {
-  logger.info({ message: `Server ready at ${url}` })
+server.applyMiddleware({ app })
+
+app.listen({ port: 4003 }, () => {
+  logger.info({ message: `🚀 Server ready` })
+})
+
+app.get("/user", async (req: Request, res: Response) => {
+  const userId = req.query.userId
+  const incomingSig = req.header("sig")
+
+  if (typeof userId !== "string") {
+    return res.sendStatus(400)
+  }
+
+  const sig = crypto.createHmac("sha256", process.env.APP_SECRET)
+    .update(userId)
+    .digest("hex");
+
+  if (sig !== incomingSig) {
+    return res.sendStatus(403)
+  }
+
+  const ctx = createContext({ req })
+
+  try {
+    const user = await ctx.prisma.user.findUnique({ where: { id: userId } })
+    if (!user) {
+      return res.sendStatus(404)
+    }
+    return res.status(200).send({ email: user.email })
+  } catch (e) {
+    return res.status(400)
+  }
 })
