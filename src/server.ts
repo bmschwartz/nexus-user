e3cfec2b0c060f4609ce664510b2816ff670a096
Ben Schwartz <schwartz.ben0@gmail.com>
2021-03-16 18:43:26 -0600

    Pull graph variant from .env

--- src/server.ts
+++ src/server.ts
@@ -19,13 +19,15 @@ AmplifyAuth.configure(awsExports)
 const app = express()
 app.use(bodyParser.json())
 
+const graphVariant = process.env.APOLLO_GRAPH_VARIANT || "current"
+
 const server = new ApolloServer({
   schema: applyMiddleware(
     buildFederatedSchema([{ typeDefs, resolvers }]),
     permissions,
   ),
   engine: {
-    graphVariant: "current",
+    graphVariant,
   },
   context: createContext,
   introspection: true,
